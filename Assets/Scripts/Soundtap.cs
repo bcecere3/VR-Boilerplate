a693335f76300b27712dce43862d1979510eb229
Pierce McBride <piercermcbride@gmail.com>
2017-02-04 21:35:23 -0900

    Working Vuforia tap and SFX trigger

---
+++ Assets/Scripts/Soundtap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Soundtap : VRTK_InteractableObject {
+
+	public AudioClip SFX;
+
+	private AudioSource source;
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource>();
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void StartTouching(GameObject currentTouchingObject){
+		base.StartTouching (currentTouchingObject);
+		Debug.Log ("Happned");
+		source.PlayOneShot(SFX,1);
+	}
+
+}
